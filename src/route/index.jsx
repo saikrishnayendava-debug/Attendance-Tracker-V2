19684f600c339fa8bf0a9020f2fe348d4f95f63f
TARUN SAI YENDAVA <tarunsaiyendava@gmail.com>
2025-11-07 14:59:23 +0530

    5

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import Login from "../Pages/Login";
 import Home from "../Pages/Home";
-import ChartComponent from "../Components/ChartComponent";
+
 const router = createBrowserRouter([
     {
         path: "/",
@@ -11,10 +11,6 @@ const router = createBrowserRouter([
     {
         path : '/home',
         element: <Home/>
-    },
-    {
-        path : '/test',
-        element: <ChartComponent/>
     }
 ])
 export default router;
