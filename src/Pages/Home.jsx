ae5d38497d873a7ec8ab8da5e089fd11140cb7a8
TARUN SAI YENDAVA <tarunsaiyendava@gmail.com>
2025-11-16 01:02:21 +0530

    s

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -448,13 +448,13 @@ const Home = () => {
 
 
             <div className='grid grid-cols-2 gap-3'>
-              <button type='submit' className='cursor-pointer bg-gray-400 text-black  rounded-2xl py-2 font-extrabold text-sm flex gap-1 items-center justify-center'>
+              <button type='submit' className='cursor-pointer bg-gray-600 text-black  rounded-lg py-2 font-extrabold text-sm flex gap-1 items-center justify-center'>
                 Submit
                 <GoGraph className=' rounded-md p-1 text-black ' size={24} />
               </button>
-              <button type='button' onClick={handleReset} className='cursor-pointer bg-gray-400 text-black rounded-2xl py-2 font-extrabold text-sm flex gap-1 items-center justify-center'>
+              <button type='button' onClick={handleReset} className='cursor-pointer bg-gray-600 text-black rounded-lg py-2 font-extrabold text-sm flex gap-1 items-center justify-center'>
                 Reset
-                <RiRefreshLine className='text-emerald-950 rounded-md' size={20} />
+                <RiRefreshLine className='text-black rounded-md' size={20} />
               </button>
             </div>
 
