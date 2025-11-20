f7271f567b1ce9889ff828a0a82912f5d7366c6c
TARUN SAI YENDAVA <tarunsaiyendava@gmail.com>
2025-11-20 19:56:45 +0530

    ui

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const FooterComponent = () => {
   return (
-    <section className='mt-17  '>
+    <section className='mt-10  '>
       <div className="flex flex-col items-center justify-center gap-4 py-5">
         {/* <div className="flex items-center gap-2">
           <p>All Study Materials</p>
           <Link to="https://btechvault.vercel.app">click</Link>
         </div> */}
-        <Link to='https://github.com/tarunsaiy' className='text-2xs text-slate-700  '>github</Link>
+        <Link to='https://github.com/tarunsaiy' className='text-2xs text-slate-700  '>github </Link>
         {/* <p className='text-xs font-bold text-pink-400/40'>#Salaar #Shouryanga</p> */}
       </div>
     </section>
