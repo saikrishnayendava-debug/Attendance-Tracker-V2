b208d2c7d92ff0ce6a20d6b2dabad27f7f6c7d52
tarunsaiy <tarunsaiyendava@gmail.com>
2025-12-30 03:09:36 +0530

    removedfooter

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const FooterComponent = () => {
   return (
     <div className='bg-black flex justify-center items-center'>
-      <p className='relative bottom-0 text-2xs text-slate-400 mt-1 text-center'>Developed by: <span className='font-bold text-slate-200 text-2xs'>Tarun Sai Krishna</span></p>
+      {/* <p className='relative bottom-0 text-2xs text-slate-400 mt-1 text-center'>Developed by: <span className='font-bold text-slate-200 text-2xs'>Tarun Sai Krishna</span></p> */}
     
     </div>
 
